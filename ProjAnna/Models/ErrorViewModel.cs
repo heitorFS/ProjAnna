029a530bab485e0c9d7c6bce9d4e3db009b4a324
heitorFS <heitor.freitasssilva@gmail.com>
2023-04-24 20:48:35 +0100

    Delete ProjAnna directory

--- ProjAnna/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjAnna.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
